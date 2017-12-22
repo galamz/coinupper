44e02025d2bea4120e080f55ae1900b16d222207
med said <ghallammed@gmail.comEnter your passwordNEXTghallammed@gmail.comEnter your passwordNEXTghallammed@gmail.comEnter your passwordNEXTghallammed@gmail.com>
2017-12-22 01:49:48 +0000

    no message

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,27 +21,25 @@ class AppServiceProvider extends ServiceProvider
 
         try{
 
-            
 
             // need cache 5 min
             $DataBar         = CryptoCurrency::selectRaw('count(id) as CryptoCurrencies, sum(volume_24h_usd) as sumVolume_24h_usd,   sum(market_cap_usd) sumCryptoCurrencies')->first();
-
             $CryptoCurrencies = $DataBar->CryptoCurrencies;
             $sumCryptoCurrencies = number_format($DataBar->sumCryptoCurrencies,2,',','.');
             $sumVolume_24h_usd = number_format($DataBar->sumVolume_24h_usd,2,',','.');
 
 
-            $BTC_price  = CryptoCurrency::whereSymbol('BTC')->orderBy('id')->firstOrFail(['price_usd']);
-            $ETH_price  = CryptoCurrency::whereSymbol('ETH')->orderBy('id')->firstOrFail(['price_usd']);
-            $BTC_price_usd = $BTC_price->price_usd;
-            $ETH_price_usd = $ETH_price->price_usd;
+            $BTC_price  = CryptoCurrency::whereSymbol('BTC')->orderBy('id')->first(['price_usd']);
+            $ETH_price  = CryptoCurrency::whereSymbol('ETH')->orderBy('id')->first(['price_usd']);
+            $BTC_price_usd = ($BTC_price ? $BTC_price->price_usd : 0 );
+            $ETH_price_usd = ($ETH_price ? $BTC_price->price_usd : 0);
 
         }catch (QueryException $exception){
-            $CryptoCurrencies = 0;
-            $sumCryptoCurrencies = 0;
-            $BTC_price_usd = 0;
-            $ETH_price_usd = 0;
-            $sumVolume_24h_usd = 0;
+            $CryptoCurrencies       = 0;
+            $sumCryptoCurrencies    = 0;
+            $BTC_price_usd          = 0;
+            $ETH_price_usd          = 0;
+            $sumVolume_24h_usd      = 0;
 
         }
 
