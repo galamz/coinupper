4216c11ce8e36e644942fb3d1f5f282e2f74788a
med said <ghallammed@gmail.comEnter your passwordNEXTghallammed@gmail.comEnter your passwordNEXTghallammed@gmail.comEnter your passwordNEXTghallammed@gmail.com>
2017-12-22 01:58:19 +0000

    no message

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\CryptoCurrency;
 use Illuminate\Database\QueryException;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
@@ -17,6 +18,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         \Schema::defaultStringLength(191);
+        URL::forceScheme('https');
 
 
         try{
